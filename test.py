7ed9159b0a92945e74e3e1f30adf7558c2be924a
梦泽 陈 <1398947448@qq.com>
2025-01-24 15:42:46 +0800

    translation

---
+++ test.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pandas as pd
+import numpy as np
+
+folder = "2025_Problem_C_Data/"
+
+
+def load(file_name, encoding="latin1"):
+    return pd.read_csv(folder + file_name, encoding=encoding)
+
+# 运动员~年份~国家~项目~奖牌
+athletes = load("summerOly_athletes.csv")
+# 举办者
+hosts = load("summerOly_hosts.csv")
+# 国家~奖牌数~年份
+medals = load("summerOly_medal_counts.csv")
+# 项目~各年次数
+programs = load("summerOly_programs.csv",encoding="windows-1252")
+
+dataframes = athletes, hosts, medals, programs
+[print(df.head(), "\n", df.shape) for df in dataframes]
