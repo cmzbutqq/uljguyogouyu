24099604935dc4010af89a64acbe891324a1c528
梦泽 陈 <1398947448@qq.com>
2025-01-25 20:39:14 +0800

    rename

---
+++ k-means.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+SEED=42
+pd.set_option('display.max_rows', None)  # 不限制行数
+# 假设df是包含上述数据的DataFrame
+df = pd.read_csv('countries.csv')  # 用实际文件路径替换
+# 选择要聚类的特征
+features = ['Events', 'Medals','Golds',  'LNM', 'CV', 'Focus']
+# 标准化特征
+X = StandardScaler().fit_transform(df[features])
+# 确定K值
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=SEED)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+# plt.savefig('plots/Elbow_Method.png')
+
+# 选择WCSS下降速度明显变缓的点作为K值
+k = 5 
+kmeans = KMeans(n_clusters=k, init='k-means++', random_state=SEED)
+df['cluster'] = kmeans.fit_predict(X)
+
+# 分析聚类结果
+# 计算每个聚类的个数
+cluster_counts = df[features+['cluster']].groupby('cluster').size()
+print("Count of each cluster:")
+print(cluster_counts)
+
+# 选择数值列
+numeric_columns = df.select_dtypes(include=[np.number]).columns.tolist()
+# 使用groupby和agg方法计算每个数值列的均值和方差
+cluster_stats = df.groupby('cluster')[numeric_columns].agg(['mean', 'std'])
+print(cluster_stats.to_string(float_format='%.2f'))
+
+
+from sklearn.decomposition import PCA
+
+# 使用PCA进行降维
+pca = PCA(n_components=2)
+df_pca = pca.fit_transform(df[features])
+# 可视化
+plt.figure()
+plt.scatter(df_pca[:, 0], df_pca[:, 1], c=df['cluster'], cmap='viridis')
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.title('2D KMeans Clustering after PCA')
+plt.show()
+# 使用PCA或t-SNE将数据降至3维
+pca = PCA(n_components=3)
+df_pca = pca.fit_transform(df[features])
+# 可视化
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(df_pca[:, 0], df_pca[:, 1], df_pca[:, 2], c=df['cluster'], cmap='viridis')
+plt.show()
+
