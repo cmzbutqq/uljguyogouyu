096867aac5ba973d5d2a48aa949f5b1224a5612c
梦泽 陈 <1398947448@qq.com>
2025-01-27 03:18:38 +0800

    first_medal

---
+++ first_medal.py
@@ -0,0 +1,42 @@
+import csv
+
+path = "2025_Problem_C_Data/summerOly_medal_counts.csv"
+
+
+def main():
+    country_first_year = {}
+    year_first_countries = {}
+
+    with open(path, "r") as file:
+        reader = csv.reader(file)
+        next(reader)  # 跳过标题行
+
+        for row in reader:
+            noc = row[1]
+            total = int(row[5])
+            if total <= 0:
+                continue  # 忽略无奖牌记录
+
+            year = int(row[6])
+            if noc not in country_first_year or year < country_first_year[noc]:
+                country_first_year[noc] = year
+
+    # 生成year_first_countries
+    for country, year in country_first_year.items():
+        if year not in year_first_countries:
+            year_first_countries[year] = []
+        year_first_countries[year].append(country)
+        
+    # 按国家名称排序并输出
+    for country in sorted(country_first_year):
+        print(f"{country},{country_first_year[country]}")
+        
+    # 按年份排序输出
+    for year in sorted(year_first_countries):
+        countries = sorted(year_first_countries[year])  # 国家按字母顺序排序
+        print(f"{year}: {', '.join(countries)}")
+    return year_first_countries,country_first_year
+
+
+if __name__ == "__main__":
+    main()
