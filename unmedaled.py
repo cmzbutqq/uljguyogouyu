36ca7105d92d17f070bf244e8cd5be5d3af3ab95
梦泽 陈 <1398947448@qq.com>
2025-01-27 03:55:58 +0800

    note

--- unmedaled.py
+++ unmedaled.py
@@ -3,8 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import first_medal
 
+"""
+2024年参加过比赛 但从未得到过奖牌的国家(Team)
+！！！这里面的队伍有很多是地区和重名，本质是队伍而非国家，注意鉴别
+"""
+
 ATHLETES = pd.read_csv("2025_Problem_C_Data/summerOly_athletes.csv")
-ATHLETES = ATHLETES[ATHLETES["Year"]==2024]
+ATHLETES = ATHLETES[ATHLETES["Year"] == 2024]
 countries = ATHLETES["Team"].unique()
 all_countries = set()
 for c in countries:
